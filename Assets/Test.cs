fb3c86b5ad97e29db1227155da85fca313daeea3
taka0358 <123725606+taka0358@users.noreply.github.com>
2023-02-06 16:16:42 +0900

    LESSON４課題再提出
    LESSON４課題再提出します。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,48 +7,38 @@ public class Test : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-       
-    
-            // 要素数5の配列を初期化する
-            int[] array = new int[5];
+        // 要素数5の配列を初期化する
+        int[] array = new int[5];
 
         // 配列の各要素に値を代入する
-            array[0] = 7;
-            array[1] = 1;
-            array[2] = 5;
-            array[3] = 3;
-            array[4] = 8;
+        array[0] = 3;
+        array[1] = 2;
+        array[2] = 5;
+        array[3] = 1;
+        array[4] = 8;
 
-            // 配列の要素をすべて表示する
-            for (int i = 0; i < 5; i++)
-            
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
 
-            {
-                Debug.Log(array[i]);
-              
-            }
-    
-       
+        }
+               
 
-        
+        // 配列要素の順番を反転させる
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // Update is called once per frame
-        void Update()
+   
+// Update is called once per frame
+void Update()
     {
-        
-    }
+      
+
+          
+        }
 }
